48118025fd0787dcf296d55063338257813fd548
Kylie <kyliepace@gmail.com>
2017-04-09 14:16:08 -0700

    server returns filtered properties

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const server = express();
 var unirest = require('unirest');
+const turfBuffer = require('@turf/buffer');
+const turfWithin = require('@turf/within');
 
 server.use(express.static(path.join(__dirname, 'public'))); //send public files to client
 server.use(bodyParser.json());
@@ -12,12 +15,52 @@ server.use(bodyParser.urlencoded({ extended: true }));
 // 	return res.sendFile(path.join(__dirname, 'public/index.html'));
 // });
 
-var filterResults = function(json) {
+var crossFits;
+fs.readFile('./geojsons/crossFits.json', 'utf8', function(err, data){
+	if(err) throw err;
+	crossFits = JSON.parse(data);
+});
 
+var filterResults = function(json) {
+	console.log(crossFits.features.length); // make sure crossFits has loaded
+	var points = {
+		'type': 'FeatureCollection',
+		'features': []
+	};
+	for(var i = 0; i < json.listing.length; i ++) {
+		var listing = json.listing[i];
+		var feature = {
+			'type' : 'Feature',
+			'properties': {
+				'street_name': listing.street_name,
+				'image_url': listing.image_80_60_url,
+				'url' : listing.details_url,
+				'category': listing.category,
+				'type': listing.property_type,
+				'country': listing.country,
+				'price': listing.price,
+				'address': listing.displayable_address,
+				'report': listing.property_report_url
+			},
+			'geometry': {
+				'type': 'Point',
+				'coordinates': [listing.longitude, listing.latitude]
+			}
+		};
+		points.features.push(feature);
+	};
+
+	
+	// use turf.within to compare set of points to a set of polygons, return points that fall within polygons
+	var searchWithin = turfBuffer(crossFits, 8, 'miles');
+	console.log(searchWithin.features[0].geometry.coordinates);
+	
+	return turfWithin(points, searchWithin);
 };
 
 server.get('/crossfits', function(req, res) {
 	return res.sendFile(path.join(__dirname, 'geojsons/crossFits.json'));
+	// could I do res.status(200).json(crossFits); instead?
 });
 // get list of properties for sale
 server.get('/zoopla', function(req, res) {
@@ -27,11 +70,10 @@ server.get('/zoopla', function(req, res) {
 	.end(function(response) {
 		reJson = response.body;
 		console.log('got res');
-		console.log(reJson);
 		// sort through the reJson.listing array, compare to crossfit locations, return matching features
 		var filteredJson = filterResults(reJson);
 		res.setHeader('Content-Type', 'application/json');
-		//res.status(200).json(reJson);
+		res.status(200).json(filteredJson);
 	});
 });
 
