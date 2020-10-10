369dcd82f0b764000ab68098b4d8017c1a16689e
Kylie <kyliepace@gmail.com>
2020-10-10 16:18:44 +0100

    add push-dir

--- front-end/nuxt.config.js
+++ front-end/nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
+  router: {
+    base: '/cfHomes/'
+  },
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
